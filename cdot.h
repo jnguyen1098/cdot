2099f504fcda4d80ca6ae3932d393241b44fd0e8
jnguyen1098 <jnguyen1098@gmail.com>
2021-03-27 16:17:54 -0400

    better linting

--- cdot.h
+++ cdot.h
@@ -3,25 +3,28 @@
 
 #include <stdio.h>
 
-#define FOURSPACE   "    "
-#define TAB         "\t"    // you monster
+#define FOURSPACE "    "
+#define TAB "\t"  // you monster
 
-#define INDENT      FOURSPACE
+#define INDENT FOURSPACE
 
 FILE *out;
 
-#define INIT_CDOT(file_out) do {                                            \
-    out = file_out;                                                         \
-    fprintf(out, "digraph G {\n");                                          \
-    fprintf(out, "%sgraph [ordering=\"out\"];\n", INDENT);                  \
-} while (0)                                                                 \
+#define INIT_CDOT(file_out)                                    \
+    do {                                                       \
+        out = file_out;                                        \
+        fprintf(out, "digraph G {\n");                         \
+        fprintf(out, "%sgraph [ordering=\"out\"];\n", INDENT); \
+    } while (0)
 
-#define ADD_EDGE(start, end) do {                                           \
-    fprintf(out, "%s\"%s\" -> \"%s\";\n", INDENT, start, end);              \
-} while (0)                                                                 \
+#define ADD_EDGE(start, end)                                       \
+    do {                                                           \
+        fprintf(out, "%s\"%s\" -> \"%s\";\n", INDENT, start, end); \
+    } while (0)
 
-#define DESTROY_CDOT() do {                                                 \
-    fprintf(out, "}\n");                                                    \
-} while (0)                                                                 \
+#define DESTROY_CDOT()       \
+    do {                     \
+        fprintf(out, "}\n"); \
+    } while (0)
 
 #endif
